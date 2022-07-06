d363211c5b00e90b11a0a065bf01e87f0afb2f4e
Theo Vora <theo.vora@gmail.com>
2022-07-06 19:56:25 -0400

    functional

---
+++ starter/AstroPizza/AstroPizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstroPizza
+//
+//  Created by Theo Vora on 7/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ starter/AstroPizza/Pizza.swift
@@ -0,0 +1,18 @@
+//
+//  Pizza.swift
+//  AstroPizza
+//
+//  Created by Theo Vora on 7/6/22.
+//
+
+import Foundation
+
+struct Pizza {
+    var name: String
+    var cheeseType: String
+    var price: Float
+    var description: String
+    var rating: Float
+    var cal: Int
+    var imageName: String
+}
